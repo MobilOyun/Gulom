2b2fb4ff991d0f352616f1db4e9290e674cf927c
FatihAKAR <fatih.akar35@gmail.com>
2016-10-29 21:54:48 +0300

    Add files via upload

---
+++ caracter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class caracter : MonoBehaviour
+{
+    public float hiz, ziplamaGucu;
+    public bool yerdeMi;
+
+    Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        rb.AddForce(Vector3.right * h * hiz);
+    }
+
+}
